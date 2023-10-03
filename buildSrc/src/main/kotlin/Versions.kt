b1303dbcc7aeabdf5ce025e866a1fcb799d1e58f
brian <brian@ezeelink.co.id>
2023-10-03 15:16:19 +0700

    update kotlin version -> 1.9.10 kotlin compilter extension -> 1.5.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,6 +3,6 @@ object Versions {
     const val COMPOSE_NAVIGATION = "2.7.3"
     const val COMPOSE_ACTIVITY = "1.7.2"
 
-    const val KOTLIN = "1.9.0"
-    const val KOTLIN_COMPILER_EXTENSION = "1.5.2"
+    const val KOTLIN = "1.9.10"
+    const val KOTLIN_COMPILER_EXTENSION = "1.5.3"
 }
