8384692ca82e4542eb72eeaa8046e25b4c630926
brian <brian@ezeelink.co.id>
2023-11-02 14:51:23 +0700

    add disable color on custom button

--- iosApp/iosApp/core/ui/view/CustomButton.swift
+++ iosApp/iosApp/core/ui/view/CustomButton.swift
@@ -9,41 +9,80 @@
 import SwiftUI
 
 struct GoldButton: ButtonStyle {
-    let clipShape = RoundedRectangle(cornerRadius: 8)
-    
     func makeBody(configuration: Configuration) -> some View {
-        let isPressed = configuration.isPressed
-        let gradient = LinearGradient(
-                    gradient: Gradient(
-                        colors: [
-                            Color(
-                                red: 241 / 255,
-                                green: 211 / 255,
-                                blue: 172 / 255
-                            ),
-                            Color(
-                                red: 229 / 255,
-                                green: 174 / 255,
-                                blue: 102 / 255
-                            ),
-                        ]
+        GoldButtonView(configuration: configuration)
+    }
+}
+
+private extension GoldButton {
+    struct GoldButtonView: View {
+        let color = LinearGradient(
+            gradient: Gradient(
+                colors: [
+                    Color(
+                        red: 241 / 255,
+                        green: 211 / 255,
+                        blue: 172 / 255
                     ),
-                    startPoint: .top,
-                    endPoint: .bottom
-                )
+                    Color(
+                        red: 229 / 255,
+                        green: 174 / 255,
+                        blue: 102 / 255
+                    ),
+                ]
+            ),
+            startPoint: .top,
+            endPoint: .bottom
+        )
+        let disableColor = LinearGradient(
+            gradient: Gradient(
+                colors: [
+                    Color(
+                        red: 216 / 255,
+                        green: 216 / 255,
+                        blue: 216 / 255
+                    ),
+                    Color(
+                        red: 182 / 255,
+                        green: 182 / 255,
+                        blue: 182 / 255
+                    ),
+                ]
+            ),
+            startPoint: .top,
+            endPoint: .bottom
+        )
         
-        return configuration.label
-            .padding()
-            .background(gradient)
-            .clipShape(clipShape)
-            .overlay(
-                clipShape
-                .stroke(Color.black, lineWidth: 1))
+        @Environment(\.isEnabled) var isEnabled
+        let configuration: GoldButton.Configuration
+        
+        var body: some View {
+            let clipShape = RoundedRectangle(cornerRadius: 8)
+            
+            return configuration.label
+                .padding()
+                .background(isEnabled ? color : disableColor)
+                .clipShape(clipShape)
+                .overlay(
+                    clipShape
+                    .stroke(Color.black, lineWidth: 1))
+                .scaleEffect(configuration.isPressed ? 1.1 : 1)
+                .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+        }
     }
 }
 
 #Preview {
-    Button("Press") {
+    VStack {
+        Button("Press") {
+            
+        }.buttonStyle(GoldButton())
         
-    }.buttonStyle(GoldButton())
+        Button("Disable") {
+            
+        }
+        .buttonStyle(GoldButton())
+        .disabled(true)
+    }
+    
 }
